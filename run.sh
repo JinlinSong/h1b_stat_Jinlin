2b858baf2c51ed7901f97e5bfaaefcaf0924c2f0
Jinlin Song <10522939+JinlinSong@users.noreply.github.com>
2018-11-11 17:12:36 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Use this shell script to compile (if necessary) your code and then execute it.
 # I'll execute my programs, with the input directory input and output the files in the directory output
 python ./src/h1b_stat.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
